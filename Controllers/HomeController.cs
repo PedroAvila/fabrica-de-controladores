0c848b3019edf73d5d9291148101ea4e85c3168d
PedroAvila <minimainor_austin@hotmail.com>
2021-05-01 17:36:17 -0500

    Creando una fábrica personalizada de controladores - parte I

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FabricaDeControladores.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger _logger;
+
+        public HomeController(): this(new DefaultLogger())
+        {
+
+        }
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            _logger.Write("En el método de acción Index...");
+            return Content("Bienvenidos!!!");
+        }
+    }
+}
